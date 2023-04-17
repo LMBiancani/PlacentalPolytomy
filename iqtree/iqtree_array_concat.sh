3437cd8c58821cb36e24fdc0378eb42f99196f99
Leann M Biancani <leann.biancani@gmail.com>
2023-04-17 13:36:25 -0400

    update concat slurm submission script

--- iqtree/iqtree_array_concat.sh
+++ iqtree/iqtree_array_concat.sh
@@ -11,27 +11,42 @@
 
 ## update array line above based on output of iqtree prep script
 
-cd $SLURM_SUBMIT_DIR
+# UPDATE:
+# path to output folder for iqtree (must be location of array_list.txt and aligned_loci_list_* created by iqtree prep script)
+array_work_folder=/data/schwartzlab/Biancani/PlacentalPolytomy/output/iqtree_assessment
+# path to FILTERED SISRS loci (aligned contigs) folder:
+aligned_loci_path=/data/schwartzlab/Biancani/PlacentalPolytomy/output/SISRS_out_filtered
+#location of iqtree executable:
+iqtree_exe="/data/schwartzlab/alex/andromeda_tools/iqtree-2.1.2-Linux/bin/iqtree2"
+# location of iqtree scripts:
+scripts_dir=/data/schwartzlab/Biancani/PlacentalPolytomy/iqtree
+# path to file containing alternative hypotheses trees:
+trees_to_eval=/data/schwartzlab/Biancani/PlacentalPolytomy/iqtree/hypothesis_trees/Polytomy_Placental_Hypotheses.tree
+# location of amas executable:
+path_to_amas="/data/schwartzlab/Biancani/AMAS/amas/AMAS.py"
+
+
+cd $array_work_folder
+mkdir -p concat_trees
+cd concat_trees
+cp --update $array_work_folder/array_list.txt .
+cp --update $array_work_folder/aligned_loci_list_* .
 
 date
-module load R/4.0.3-foss-2020b
 
-# first need to copy 
-# cp ../iqtree_phyloinference/array_list.txt .
-# and
-# cp ../iqtree_phyloinference/aligned_loci_list_* .
+module purge
+module load Python/3.7.4-GCCcore-8.3.0
 
 fileline=$(sed -n ${SLURM_ARRAY_TASK_ID}p array_list.txt)
-aligned_loci_path="/home/aknyshov/alex_data/tree_shew_analysis/SISRS/post_processing/aligned_loci/"
-iqtree_exe="/home/aknyshov/alex_data/andromeda_tools/iqtree-2.1.2-Linux/bin/iqtree2"
-# trees_to_eval="/home/aknyshov/alex_data/tree_shew_analysis/SISRS/post_processing/treeshrew_trees_to_fit.tre"
-trees_to_eval="/home/aknyshov/alex_data/tree_shew_analysis/SISRS/post_processing/primates_trees_to_fit.tre"
-scripts_dir="/home/aknyshov/alex_data/tree_shew_analysis/TreeshrewProject/iqtree/"
 
-
+# generates list of paths to infiles
 infiles=$(cat ${fileline} | while read line; do echo ${aligned_loci_path}/${line}; done | paste -sd" ")
 
-python3 ~/alex_data/andromeda_tools/AMAS/amas/AMAS.py concat -f fasta -d dna --out-format fasta --part-format raxml -i $infiles -t concatenated_${SLURM_ARRAY_TASK_ID}.fasta -p partitions_${SLURM_ARRAY_TASK_ID}.txt
+#amas concatenated
+python3 ${path_to_amas} concat -f fasta -d dna --out-format fasta --part-format raxml -i $infiles -t concatenated_${SLURM_ARRAY_TASK_ID}.fasta -p partitions_${SLURM_ARRAY_TASK_ID}.txt
+
+module purge
+module load R/4.0.3-foss-2020b
 
 Rscript ${scripts_dir}trimTrees.R concatenated_${SLURM_ARRAY_TASK_ID}.fasta ${trees_to_eval} ./trees_${SLURM_ARRAY_TASK_ID}.tre
 
