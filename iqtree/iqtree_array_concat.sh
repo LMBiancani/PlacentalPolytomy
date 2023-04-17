4b2cf45124f07f9a5091b559997b71a326b332aa
Leann M Biancani <leann.biancani@gmail.com>
2023-04-17 13:50:21 -0400

    update base on Zack's changes

--- iqtree/iqtree_array_concat.sh
+++ iqtree/iqtree_array_concat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="IQarr"
+#SBATCH --job-name="IQcat"
 #SBATCH --time=24:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=1   # processor core(s) per node
@@ -50,7 +50,18 @@ module load R/4.0.3-foss-2020b
 
 Rscript ${scripts_dir}trimTrees.R concatenated_${SLURM_ARRAY_TASK_ID}.fasta ${trees_to_eval} ./trees_${SLURM_ARRAY_TASK_ID}.tre
 
-${iqtree_exe} -nt 10 -s concatenated_${SLURM_ARRAY_TASK_ID}.fasta -spp partitions_${SLURM_ARRAY_TASK_ID}.txt -z ./trees_${SLURM_ARRAY_TASK_ID}.tre -pre calcLnL_${SLURM_ARRAY_TASK_ID} -n 0 -m GTR+G -wsl
-${iqtree_exe} -nt 10 -s concatenated_${SLURM_ARRAY_TASK_ID}.fasta -spp partitions_${SLURM_ARRAY_TASK_ID}.txt -pre inference_${SLURM_ARRAY_TASK_ID} -m GTR+G -bb 1000 -alrt 1000 -wsr
+#${iqtree_exe} -nt 10 -s concatenated_${SLURM_ARRAY_TASK_ID}.fasta -spp partitions_${SLURM_ARRAY_TASK_ID}.txt -z ./trees_${SLURM_ARRAY_TASK_ID}.tre -pre calcLnL_${SLURM_ARRAY_TASK_ID} -n 0 -m GTR+G -wsl
+#${iqtree_exe} -nt 10 -s concatenated_${SLURM_ARRAY_TASK_ID}.fasta -spp partitions_${SLURM_ARRAY_TASK_ID}.txt -pre inference_${SLURM_ARRAY_TASK_ID} -m GTR+G -bb 1000 -alrt 1000 -wsr
+
+sed -n 1p ./trees_${SLURM_ARRAY_TASK_ID}.tre > ./tree1_${SLURM_ARRAY_TASK_ID}.tre
+${iqtree_exe} -nt 10 -s concatenated_${SLURM_ARRAY_TASK_ID}.fasta -spp partitions_${SLURM_ARRAY_TASK_ID}.txt -pre calcLnL1_${SLURM_ARRAY_TASK_ID} -g ./tree1_${SLURM_ARRAY_TASK_ID}.tre -m GTR+G -redo -wsl
+
+sed -n 2p ./trees_${SLURM_ARRAY_TASK_ID}.tre > ./tree2_${SLURM_ARRAY_TASK_ID}.tre
+${iqtree_exe} -nt 10 -s concatenated_${SLURM_ARRAY_TASK_ID}.fasta -spp partitions_${SLURM_ARRAY_TASK_ID}.txt -pre calcLnL2_${SLURM_ARRAY_TASK_ID} -g ./tree2_${SLURM_ARRAY_TASK_ID}.tre -m GTR+G -redo -wsl
+
+sed -n 3p ./trees_${SLURM_ARRAY_TASK_ID}.tre > ./tree3_${SLURM_ARRAY_TASK_ID}.tre
+${iqtree_exe} -nt 10 -s concatenated_${SLURM_ARRAY_TASK_ID}.fasta -spp partitions_${SLURM_ARRAY_TASK_ID}.txt -pre calcLnL3_${SLURM_ARRAY_TASK_ID} -g ./tree3_${SLURM_ARRAY_TASK_ID}.tre -m GTR+G -redo -wsl
+
+${iqtree_exe} -nt 10 -s concatenated_${SLURM_ARRAY_TASK_ID}.fasta -spp partitions_${SLURM_ARRAY_TASK_ID}.txt -pre inference_${SLURM_ARRAY_TASK_ID} -m GTR+G -bb 1000 -alrt 1000 -redo -wsr
 
 date
