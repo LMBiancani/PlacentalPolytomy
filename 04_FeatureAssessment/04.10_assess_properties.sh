d944035973ddda23bdb468d64023352e57f2a3bb
Leann M Biancani <leann.biancani@gmail.com>
2025-04-11 11:52:50 -0400

    update 04.10 submission script and notes

--- 04_FeatureAssessment/04.10_assess_properties.sh
+++ 04_FeatureAssessment/04.10_assess_properties.sh
@@ -9,16 +9,16 @@
 #SBATCH --mail-type=ALL
 
 # Update Paths:
-out=/data/schwartzlab/Biancani/Phylo_ML/output
-wd=/data/schwartzlab/Biancani/Phylo_ML/01_feature_assessment
+out=/data/schwartzlab/Biancani/PlacentalPolytomy/output/04_feature_assessment
+wd=/data/schwartzlab/Biancani/PlacentalPolytomy/04_feature_assessment
 assesser_path=$wd/scripts/assess_gene_properties.R
-outfile=ML_data.txt
+outfile=properties.txt
 all_loci_path=$out/all_loci
 
 module load R-bundle-Bioconductor/3.16-foss-2022b-R-4.2.2
-date
+date 
 
-for subset in $out/subset_0[89]; do
+for subset in $out/subset_*; do
   # Update Paths to input files:
 	aligned_loci_path=$subset/alignments
   inferred_gene_trees=$subset/inferred_gene_trees.tre
@@ -27,7 +27,7 @@ for subset in $out/subset_0[89]; do
   amas_output=$subset/amas_output.txt
   rate_assessment_path=$subset/rate_assessment
   fastsp_output=$subset/fastsp_output.csv
-
+  
   cd $subset
   pwd
   Rscript ${assesser_path} ${aligned_loci_path}/ ${inferred_gene_trees} ${gene_tree_names} ${pruned_trees_path} ${amas_output} ${rate_assessment_path}/ ${outfile} ${fastsp_output}
@@ -46,3 +46,4 @@ tail -n +2 -q $out/subset_*/$outfile >> "$all_out"
 echo "Combined file created at: $all_out"
 
 date
+
